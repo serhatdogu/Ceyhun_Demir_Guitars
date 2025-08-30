4d571cd01fbd83753ee96a7d2adfed111e1d1996
Serhat Dogu <serhatdogu@gmail.com>
2025-08-30 18:17:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let slideIndex = 0;
+    showSlides();
+  
+    function showSlides() {
+      const slides = document.getElementsByClassName("slide");
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+      slides[slideIndex - 1].style.display = "block";
+      setTimeout(showSlides, 4000);
+    }
+  });
+  document.addEventListener("DOMContentLoaded", showSlides);
